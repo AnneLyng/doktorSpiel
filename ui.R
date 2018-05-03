8245164e538f03c0e4bbae690dcc708769747ba7
AnneLyng <lynganne@gmail.com>
2018-05-03 16:01:31 +0200

    new version

--- ui.R
+++ ui.R
@@ -6,11 +6,11 @@ dashboardPage(
     dashboardSidebar(disable = T),
     dashboardBody(box(width=12,
                       tabBox(width=12, id="tabBox_next_previous",
-                             tabelPanel("Introduction",
+                             tabPanel("Introduction",
                                         p("Introduction to the doktorSpiel")),
-                             tabelPanel("Initial treatment schemes",
+                             tabPanel("Initial treatment schemes",
                                         p("Inital treatment schemes for patients based on disease")),
-                             tabelPanel("The game - treatment process simulator",
+                             tabPanel("The game - treatment process simulator",
                                         p("The game"))
                              ),
                       uiOutput("NextPrevious")
