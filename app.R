999ab1723990807d859e7bafea0449448ee7d9cd
annelyng <lynganne@gmail.com>
2018-05-03 11:05:02 +0200

    added start page - TODO: merge with app

--- app.R
+++ app.R
@@ -1,18 +1,29 @@
-library(shiny)
+## Author: Anne Lyngholm Soerensen (lynganne@gmail.com)
+## Research assistant at the section of Biostatistics at the UniCopenhagen
+## Date: 03-05-2018
+## Description: This app contains the main application. It is expected that the user will
+# have entered some start arguments/conditions under which the next lines of
+# codes will be generated. 
 
-ui <- fluidPage(
+library(shiny)
+uiMain <- fluidPage(
     # header
     headerPanel("doktorSpiel"),
-
+    
     # sidebar for adjusting the dosis
-    sidebarPanel(),
+    sidebarLayout(
+        sliderInput("timeSpan", "Running the treatment Plan",
+                    min=0, max=100, value = 1, step = 5,
+                    animate = animationOptions(interval = 300,
+                                               loop = TRUE,
+                                               playButton="start",
+                                               pauseButton="pause")
+    ),
 
     # main panel for the process of the individual in the treatment
-    mainPanel(),
+    mainPanel())
 
     # create action buttons (pause and play)
-    actionButton("start","Start"),
-    actionButton("pause","Pause")
     )
 
 server <- function(input, output){
@@ -20,6 +31,3 @@ server <- function(input, output){
 }
 
 shinyApp(ui,server)
-
-    
-
